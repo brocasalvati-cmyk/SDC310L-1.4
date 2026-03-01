0a9d45af881090b17683bbe5e0ccfff870e69761
brocasalvati-cmyk <broc.a.salvati@gmail.com>
2026-03-01 08:51:28 -0500

    Update AddressView.php

--- AddressView.php
+++ AddressView.php
@@ -1,8 +1,22 @@
 <table>
-    <?php while($row = $addresses->fetch_assoc()): ?>
-    <tr>
-        <td><?php echo $row['First'] . " " . $row['Last']; ?></td>
-        <td><a href="?action=delete&id=<?php echo $row['AddressNo']; ?>">Delete</a></td>
-    </tr>
-    <?php endwhile; ?>
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Actions</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php while($row = $addresses->fetch_assoc()): ?>
+        <tr>
+            <td><?php echo htmlspecialchars($row['First'] . " " . $row['Last']); ?></td>
+            
+            <td>
+                <a href="?action=delete&id=<?php echo $row['AddressNo']; ?>" 
+                   onclick="return confirm('Are you sure you want to delete this record?');">
+                   Delete
+                </a>
+            </td>
+        </tr>
+        <?php endwhile; ?>
+    </tbody>
 </table>
